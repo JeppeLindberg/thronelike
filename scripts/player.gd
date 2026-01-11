b58f4ee8bbe7fb784b2e082300c722af7264c089
Jeppe Lindberg <jepp3441@gmail.com>
2026-01-11 20:24:16 +0100

    progress

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,9 +20,8 @@ var input_pick_up = false
 var move_direction = Vector2.ZERO
 var bullet_timer = 0.0
 var current_ground_item = null
-var weapon_1 = null
-var weapon_2 = null
-var current_weapon_index = 0
+var weapons = [null, null]
+var current_weapon_index = -1
 
 const SPEED = 50.0
 const ACCEL = 1000.0
@@ -32,8 +31,6 @@ const ACCEL = 1000.0
 func _ready() -> void:
 	var weapon = starting_weapon.instantiate()
 	pick_up_weapon(weapon)
-	weapon_1 = weapon
-	current_weapon_index = 1
 
 func _physics_process(delta: float) -> void:
 	_handle_input()
@@ -53,12 +50,8 @@ func _handle_input():
 	input_pick_up = Input.is_action_just_pressed('pick_up')
 
 func _handle_shooting(delta):
-	var current_weapon = null
-	if current_weapon_index == 1:
-		current_weapon = weapon_1
-	elif current_weapon_index == 2:
-		current_weapon = weapon_2
-	else:
+	var current_weapon = weapons[current_weapon_index]
+	if current_weapon == null:
 		return
 
 	current_weapon.shooting = input_shooting
@@ -98,24 +91,50 @@ func _set_current_ground_item(new_target):
 
 
 func pick_up_weapon(weapon):
-	if weapon_1 == null:
+	if weapons[0] == null:
 		if weapon.get_parent():
 			weapon.reparent(ui_weapon_1)
 		else:
 			ui_weapon_1.add_child(weapon)
-		weapon_1 = weapon
-		current_weapon_index = 1
+		weapons[0] = weapon
+		set_current_weapon(0)
 
-	elif weapon_2 == null:
+	elif weapons[1] == null:
 		if weapon.get_parent():
 			weapon.reparent(ui_weapon_2)
 		else:
 			ui_weapon_2.add_child(weapon)
-		weapon_2 = weapon
-		current_weapon_index = 2
+		weapons[1] = weapon
+		set_current_weapon(1)
 
 	else:
-		print('DROP CURRENT WEAPON')
+		weapons[current_weapon_index].is_current_weapon = false
+		weapons[current_weapon_index].turn_to_ground_item(global_position)
+		
+		var target_ui = null
+		if current_weapon_index == 0:
+			target_ui = ui_weapon_1
+		elif current_weapon_index == 1:
+			target_ui = ui_weapon_2
+
+		if weapon.get_parent():
+			weapon.reparent(target_ui)
+		else:
+			target_ui.add_child(weapon)
+
+		weapons[current_weapon_index] = weapon
+		set_current_weapon(current_weapon_index)
 	
 	weapon.position = Vector2.ZERO
+
+func set_current_weapon(new_weapon_index):
+	for weapon in weapons:
+		if weapon != null:
+			weapon.is_current_weapon = false
+	
+	current_weapon_index = new_weapon_index
+
+	if weapons[current_weapon_index] != null:
+		weapons[current_weapon_index].is_current_weapon = true
+
 		
--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -4,6 +4,9 @@ extends Node2D
 @onready var main = get_node('/root/main')
 @onready var world = get_node('/root/main/world')
 @onready var mouse_follower = get_node('/root/main/mouse_follower')
+@onready var ui_visual = get_node('ui_visual')
+@onready var ground_item_visual = get_node('ground_item_visual')
+@onready var reload_progress = get_node('ui_visual/reload_progress')
 
 @export var bullets_per_second = 3.0
 
@@ -11,25 +14,35 @@ extends Node2D
 
 
 var initialized = false
+var is_current_weapon = false
 var shooting = false
 var bullet_timer = 0.0
 
 
 func pick_up(player):
 	player.pick_up_weapon(self)
+	ui_visual.visible = true
+	ground_item_visual.visible = false
+
+func turn_to_ground_item(new_position, _new_velocity = Vector2.ZERO):
+	var new_ground_item = main.ground_item_prefab.instantiate()
+	world.add_child(new_ground_item)
+	new_ground_item.global_position = new_position
+	new_ground_item.add_item(self)
+	ui_visual.visible = false
+	ground_item_visual.visible = true
+
+	initialized = true
 
 func _process(_delta: float) -> void:
 	if not initialized:
 		if get_parent() == world:
-			var new_ground_item = main.ground_item_prefab.instantiate()
-			world.add_child(new_ground_item)
-			new_ground_item.global_position = global_position
-			new_ground_item.add_item(self)
-			initialized = true
-
+			turn_to_ground_item(global_position)
 
 func handle_shooting(player, delta):
 	bullet_timer += bullets_per_second * delta
+
+	reload_progress.text = str(clampf(roundf(bullet_timer * 10.0)/10.0, 0.0, 1.0))
 
 	if shooting == true:
 		if bullet_timer > 1.0:
